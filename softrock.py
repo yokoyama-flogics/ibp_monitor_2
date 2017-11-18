c25274bfaa2935a22b1dd65947d0aa1a45722a5b
Atsushi Yokoyama <yokoyama@flogics.com>
2017-11-18 21:35:22 +0900

    Writing code to set receiving frequency of SoftRock

--- softrock.py
+++ softrock.py
@@ -14,12 +14,17 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from lib.common import eprint
 
-def get_version():
+def softrock_handle():
+    """
+    Return USB handler talking to SoftRock
+    """
     context = usb1.USBContext()
-    handle = context.openByVendorIDAndProductID(
+    return context.openByVendorIDAndProductID(
         VENDOR_ID, PRODUCT_ID, skip_on_error=True)
 
-    version = handle.controlRead(
+def get_version():
+    # Refer https://pe0fko.nl/SR-V9-Si570/
+    version = softrock_handle().controlRead(
         usb1.TYPE_VENDOR | usb1.RECIPIENT_DEVICE | usb1.ENDPOINT_IN,
         0,
         0xe00,
@@ -31,10 +36,29 @@ def get_version():
     minor = version[0]
     return major, minor
 
+def set_freq(freq_hz, debug=False):
+    """
+    Set SoftRock to the specified freq_hz
+    If attached SoftRock has old firmware, we need special cares.
+    """
+    if get_version()[0] < 15:
+        # This version may not have BPF automatic setting.
+        if debug:
+            print 'This SoftRock may not have BPF automatic setting.'
+            print 'Using a manual way.'
+
 def softrock(debug=False):
-    if debug:
+    try:
         ver = get_version()
+    except:
+        eprint('ERROR: Confirm SoftRock is attached to a USB port and also you'
+            ' have a privilege\n       to access the USB port.')
+        sys.exit(1)
+
+    if debug:
         print 'Attached SoftRock firmware is version %d.%d' % ver
+
+    set_freq(100000, debug)
 
 def main():
     import argparse

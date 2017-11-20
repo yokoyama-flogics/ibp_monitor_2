87f030b10c27d34b438c5857e406f082546e4208
Atsushi Yokoyama <yokoyama@flogics.com>
2017-11-20 13:26:02 +0900

    Confirmed working with the old SoftRock v9 Lite and PCM2902
    My conclusion is, SoftRock output is, L ch = Q, R ch = I.

--- sigrec_softrock.py
+++ sigrec_softrock.py
@@ -38,79 +38,13 @@ def exceeded_sigfiles_limit():
     else:
         return False
 
-def record_one_file(datestr, timestr, line, skip_if_exist=False, debug=False):
-    """
-    Record (or 'convert' in the migration recorder case) one file from
-    the raw file specified by 'datestr' and 'line' in the file.
-    Note that the 'line' is true line number of the file.  Comment line is also
-    counted.  And return True.
-    Return false if signal file already existed.
-    """
-    from lib.config import BeaconConfigParser
-    from lib.fileio import getpath_signalfile
-    from sigretr import retrieve_signal, write_wav_file, adjust_len
-    import os
-    import wave
-
-    if not hasattr(record_one_file, 'n_samples'):
-        record_one_file.n_samples = \
-            LEN_INPUT_SEC * BeaconConfigParser().getint('Signal', 'samplerate')
-
-    filename = datestr + '/' + timestr + '.wav'
-    filepath = getpath_signalfile(datestr + '/' + timestr + '.wav')
-
-    # If the signal file exists and can be ignored, skip file retrieval
-    try:
-        if skip_if_exist and \
-                wave.open(filepath, 'rb').getnframes() == \
-                record_one_file.n_samples:
-            return False
-    except IOError as err:
-        if err[1] == 'No such file or directory':
-            # File does not exist...
-            pass
-        else:
-            raise
-    except:
-        raise
-
-    # Read signal data from raw file, and write it as .wav file
-    sig = retrieve_signal(datestr, line, debug=False)
-    sig = adjust_len(sig)
-    write_wav_file(filename, sig, to_signal_dir=True)
-
-    return True
-
-def nextday_datestr(datestr):
-    """
-    Return the next day's datestr of the given datestr.
-    20171101 will be returned if 20171031 was given, for example.
-    """
-    from datetime import datetime, timedelta
-
-    nextday = datetime.strptime(datestr, '%Y%m%d') + timedelta(days=1)
-    return nextday.strftime('%Y%m%d')
-
-def register_db(datestr, timestr, mhz, ignore_err=False, debug=False):
+def register_db(datetime_sec):
     """
     Register record information to database and return True.
     Return false if a duplicate record was found.
     """
-    from datetime import datetime
     from lib.config import BeaconConfigParser
     from lib.fileio import connect_database
-    from lib.ibp import mhz_to_freq_khz
-    from sqlite3 import IntegrityError
-
-    # Convert datestr and timestr to seconds from epoch
-    datetime_utc = datetime.strptime(
-        datestr + ' ' + timestr, '%Y%m%d %H%M%S')
-    seconds_from_epoch = int(
-        (datetime_utc - datetime.utcfromtimestamp(0)).total_seconds())
-    if debug:
-        print "seconds_from_epoch:", seconds_from_epoch
-    if seconds_from_epoch % 10 != 0:
-        raise Exception('seconds_from_epoch is not multiple of 10 seconds')
 
     # Obtain parameters from configuration
     config = BeaconConfigParser()
@@ -118,132 +52,20 @@ def register_db(datestr, timestr, mhz, ignore_err=False, debug=False):
     conn = connect_database()
     c = conn.cursor()
 
-    err_occurred = False
-    try:
-        c.execute('''INSERT INTO
-            received(datetime, offset_ms, freq_khz, bfo_offset_hz, recorder)
-            VALUES(?,?,?,?,?)''',
-            (
-                seconds_from_epoch,
-                config.getint('Migration', 'offset_ms'),
-                mhz_to_freq_khz(mhz),
-                config.getint('Migration', 'bfo_offset_hz'),
-                config.get('Migration', 'recorder')
-            ))
-        conn.commit()
-    except IntegrityError as err:
-        if ignore_err and \
-                err[0] == 'UNIQUE constraint failed: received.datetime':
-            err_occurred = True
-        else:
-            raise
+    print 'register_db: %d' % (datetime_sec)
+    c.execute('''INSERT INTO
+        received(datetime, offset_ms, freq_khz, bfo_offset_hz, recorder)
+        VALUES(?,?,?,?,?)''',
+        (
+            datetime_sec,
+            OFFSET_MS,
+            datetime_sec_to_freq_khz(datetime_sec),
+            config.getint('SignalRecorder', 'bfo_offset_hz'),
+            config.get('SignalRecorder', 'recorder')
+        ))
 
+    conn.commit()
     conn.close()
-    return not err_occurred
-
-def old_startrec(check_limit=False, debug=False):   # XXX
-    """
-    Repeat signal conversion from 'arg_from'.
-    If arg_from is 'new', it start from new line of today's file.
-    Otherwise, treats arg_from as datestr (e.g. 20171028)
-    """
-    from datetime import datetime
-    from lib.fileio import open_db_file
-    import math
-    import re
-    import time
-
-    if arg_from == 'new':
-        datestr = datetime.utcnow().strftime('%Y%m%d')
-        seek_to_tail = True
-    elif arg_from == 'today':
-        datestr = datetime.utcnow().strftime('%Y%m%d')
-        seek_to_tail = False
-    else:
-        datestr = arg_from
-        seek_to_tail = False
-
-    curfd = None
-    curline = 0     # will be initialized in the loop below in anyway
-
-    while True:
-        # For first iteration or curfd is closed, open a new file
-        if curfd is None:
-            curfd = open_db_file('ibprec_%s.txt' % (datestr), 'r')
-            curline = 0
-
-        # If seek_to_tail is True, it means starting from new line (next of the
-        # current last line, so skip already existing lines.
-        if seek_to_tail == True:
-            seek_to_tail = False    # never seek again
-
-            if debug:
-                print "Skipping to tail of the file..."
-
-            while True:
-                line = curfd.readline()
-                if line == '':
-                    break       # no more lines
-
-                curline += 1
-
-        # Now, wait for a line from curfd
-        while True:
-            line = curfd.readline()
-            if line == '':
-                time.sleep(0.5)     # sleep for a short period
-            else:
-                curline += 1
-                break
-
-        line = line.rstrip()
-
-        # Check if the line is a comment-only line
-        m = re.match(r' *#', line)
-        if m:
-            if debug:
-                print "COMMENT:", line
-            continue
-
-        # Check if the line is an end-of-file marker
-        m = re.search(r'MHz\s*$', line)
-        if not m:
-            if debug:
-                print "TERMINATED:", line
-            curfd.close()
-            curfd = None
-            curline = 0
-            datestr = nextday_datestr(datestr)
-            continue
-
-        # This is not required.  %H:%M:%S is included in the line...
-        # m = re.match(r'\d+', line)
-        # utctime = datetime.utcfromtimestamp(
-        #     math.floor((float(m.group(0)) + 6.0) / 10.0) * 10.0)
-
-        # Extract time time string from the line, and convert to %H%M%S.
-        m = re.search(r' (\d{2}):(\d{2}):(\d{2}) ', line)
-        timestr = m.group(1) + m.group(2) + m.group(3)
-
-        # Extract frequency (kHz) from the line
-        m = re.search(r' (\d+)MHz\s*$', line)
-        mhz = int(m.group(1))
-
-        if check_limit:
-            # Check if generated signal files are too much
-            if exceeded_sigfiles_limit():
-                eprint("Signal files limit (number of size) is exceeded.")
-                eprint("Waiting until not meeting the condition again.")
-            while exceeded_sigfiles_limit():
-                time.sleep(0.5)
-
-        # Finally process the line
-        status1 = record_one_file(datestr, timestr, curline, ignore_err, debug)
-        status2 = register_db(datestr, timestr, mhz, ignore_err, debug=debug)
-
-        # If some processes were required, show the progress
-        if status1 or status2:
-            print line
 
 def sec_x10(time):
     """
@@ -309,8 +131,10 @@ def output_signal(datetime_sec, samples, samplerate):
         lag = - lrlag
         ch_L[0 : n_samples - lag] = ch_R[lag : n_samples]
 
-    ch_I = ch_L
-    ch_Q = ch_R
+    # XXX   L/R from 12:33 JST Nov/20
+    # XXX   R/L from 12:58 JST Nov/20
+    ch_I = ch_R     # XXX   L/R from 12:33 JST Nov/20
+    ch_Q = ch_L     # XXX
 
     out_samples = np.column_stack((ch_I, ch_Q)).flatten()
     bytes = bytearray(out_samples)
@@ -358,6 +182,7 @@ class CutOutSamples:
                         self.head_time,
                         time)
 
+                    # Not rounding up
                     datetime_sec = int(
                         (time - datetime(1970, 1, 1)).total_seconds())
                     datetime_sec = (datetime_sec / 10) * 10
@@ -367,7 +192,7 @@ class CutOutSamples:
                         (start_sample + self.samplerate * 10) * 2 * 2], \
                         # 2 ch * S16_LE
                         self.samplerate)
-                    # register
+                    register_db(datetime_sec)
 
                 self.samples = samples
                 self.head_time = self.lasttime
@@ -398,31 +223,39 @@ class SigProc(Process):
                 eprint('Interrupted by user.  Aborted.')
                 break
 
+def roundup_time_10sec(time):
+    from datetime import datetime
+
+    datetime_sec = int((time - datetime(1970, 1, 1)).total_seconds())
+    return (datetime_sec / 10 + 1) * 10
+
+def datetime_sec_to_freq_khz(datetime_sec, debug=False):
+    from lib.ibp import mhz_to_freq_khz
+    minute = (datetime_sec / 60) % 60
+    band_idx = (minute % 15) / 3
+
+    if debug:
+        print 'datetime_sec_to_freq_khz: minute=%d' % (minute)
+
+    return mhz_to_freq_khz({
+        0: 14,
+        1: 18,
+        2: 21,
+        3: 24,
+        4: 28
+    }[band_idx])    # XXX for debug
+
 def change_freq(time, bfo_offset_hz, debug):
     """
     Tune receiver to the next receiving frequency
     Note that frequency for the next coming 10 sec boundary
     """
-    from datetime import datetime
     from softrock import set_freq
 
-    datetime_sec = int((time - datetime(1970, 1, 1)).total_seconds())
-    datetime_sec = (datetime_sec / 10 + 1) * 10
-
-    minute = (datetime_sec / 60) % 60
-    band_idx = (minute % 15) / 3
-
-    freq_base_khz = {
-        0: 14100,
-        1: 18110,
-        2: 21150,
-        3: 24930,
-        4: 28200
-    }[band_idx]
+    freq_base_khz = datetime_sec_to_freq_khz(roundup_time_10sec(time), debug)
 
     if debug:
-        print 'Changing frequency: minute=%d, freq_base_khz=%d' % \
-            (minute, freq_base_khz)
+        print 'Changing frequency: %d kHz' % (freq_base_khz)
 
     freq = freq_base_khz * 1000 - bfo_offset_hz
     set_freq(freq, debug=False)
